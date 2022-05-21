fd0a9616a71159ea91df156576a9305ba8220522
SovanRoy-coder <sovanndxalpha15@gmail.com>
2022-05-21 19:41:35 +0530

    Added project files

---
+++ SpringMVCmicro/src/main/java/com/example/springmicroservices/SpringMVCmicro/SpringMvCmicroApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmicroservices.SpringMVCmicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvCmicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvCmicroApplication.class, args);
+	}
+
+}
---
+++ SpringMVCmicro/src/test/java/com/example/springmicroservices/SpringMVCmicro/SpringMvCmicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmicroservices.SpringMVCmicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvCmicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
